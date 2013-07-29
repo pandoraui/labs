a8a41b7b10fa3fe1684280e9e0891f2affcf1d7e
cloudai <aloudai@qq.com>
2013-07-29 18:56:13 +0800

    规划v4版本

---
+++ v4/common/meta.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="dns-prefetch" href="//pic.lvmama.com">
+<link rel="dns-prefetch" href="//s1.lvjs.com.cn">
+<link rel="dns-prefetch" href="//s2.lvjs.com.cn">
+<link rel="dns-prefetch" href="//s3.lvjs.com.cn">
+<title><?php echo $title; ?></title>
+<link rel="shortcut icon" type="image/x-icon" href="http://www.lvmama.com/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="页面关键字">
+<meta name="description" content="页面描述">
+
+<!-- 生产线使用样式
+
+// 通用样式
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v3/pa-base.css,/styles/v3/pa-common.css,/styles/v3/pa-modules.css" >
+
+// 页面样式
+<link rel="stylesheet" href="propage.css">
+
+-->
+
+<!-- pa-base.css -->
+<link rel="stylesheet" href="css/base/reset.css" >
+<link rel="stylesheet" href="css/base/combo.css" >
+
+<!-- pa-common.css -->
+<link rel="stylesheet" href="css/lv-common.css" >
+
+<!-- pa-modules.css -->
+<link rel="stylesheet" href="css/modules/buttons.css" >
+<link rel="stylesheet" href="css/modules/ui-components.css">
+<link rel="stylesheet" href="css/modules/form.css" >
+
+<!-- propage.css 项目页面样式 -->
+<link rel="stylesheet" href="css/propage.css" >
---
+++ v4/php.php
@@ -0,0 +1,20 @@
+<?php $page = "start" ?>
+<?php $title="开始使用"?>
+<?php include("common/meta.php"); ?>
+
+<!-- 临时使用的内嵌样式style，不要上线此style -->
+<style>
+
+</style>
+
+</head>
+<body>
+<?php include("common/header.html"); ?>
+
+
+
+
+<?php include("common/footer.html"); ?>
+
+</body>
+</html>
