b46d08bc1147f475a8dfa0d6f15c4dddf7da7067
cloudAi <1395093509@qq.com>
2014-01-20 21:11:30 +0800

    修改

---
+++ v5/dpro-list.php
@@ -0,0 +1,92 @@
+
+<div class="dpro-list">
+    <table class="ptable table-full">
+        <thead class="pttit">
+            <tr>
+                <td></td>
+                <td>
+                    <dl class="ptditem">
+                        <dd class="pdpaytype">支付方式</dd>
+                        <dd class="pdlvprice">驴妈妈价</dd>
+                        <dd class="pdprice">市场价</dd>
+                        <dt class="pdname">产品名称</dt>
+                    </dl>
+                </td>
+            </tr>
+        </thead>
+        <tbody id="destorder" class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>单门票</h5></div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem.php"); ?>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+        <tbody class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>多人套餐</h5></div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem.php"); ?>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+        <tbody class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>组合套餐</h5></div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem.php"); ?>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+        <tbody class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>娱乐<br>餐饮<br>交通接驳</h5></div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem.php"); ?>
+                    </div>
+                </td>
+            </tr>
+        </tbody><!--//#destorder-->
+        <tbody id="freetour" class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>自由行</h5><p>景点+酒店</p></div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem2.php"); ?>
+                        <a href="#" class="link-more">查看 24 条自由行&gt;&gt;</a>
+                    </div>
+                </td>
+            </tr>
+        </tbody><!--//#freetour-->
+        <tbody id="tuangou" class="ptbox">
+            <tr>
+                <td class="ptdname">
+                    <div class="ptname"><h5>跟团游</h5><p>上海出发</p>
+                    </div>
+                </td>
+                <td>
+                    <div class="ptdlist">
+                        <?php include("ptditem2.php"); ?>
+                    </div>
+                </td>
+            </tr>
+        </tbody><!--//#tuangou-->
+    </table>
+</div><!--//.dpro-list-->
---
+++ v5/ptditem.php
@@ -0,0 +1,118 @@
+
+<dl class="ptditem">
+    <dd class="pdpaytype">
+        <span class="ipay ipay-online"><i class="itype">在线支付</i><a href="#" class="btn btn-w btn-small btn-orange">预订</a></span>
+    </dd>
+    <dd class="pdlvprice">
+        <dfn>&yen;<i>175</i></dfn>
+        <span class="tagsback"><em>返</em><i>¥20</i></span>
+    </dd>
+    <dd class="pdprice"><del>&yen;288</del></dd>
+    <dt class="pdname">
+        <a href="#" class="ptlink">恐龙园 - 成人票（12.1-12.31）</a>
+        <span class="tags tags-lightblue">折扣</span>
+    </dt>
+    <dd class="pdetails">
+        <div class="tiptext tip-light">
+            <div class="tip-arrow tip-arrow-11">
+                <em>◆</em>
+                <i>◆</i>
+            </div>
+            <div class="tip-other">
+                <dl class="dl-hor">
+                    <dt>入园须知</dt>
+                    <dd>1.营业时间：8:00—17:00（16:00停止换票）
+                    取票时间：8：30—17：00<br>
+                    2.景区地址：上海市浦东新区南六公路178号取票地点：上海市松江佘山上海欢乐谷检票处电子门票专用通道。<br>
+                    3.入园凭证：刷二维码短信入园。短信游玩当日有效，不能删除、不能转发。如您在出行前一天17：00尚未收到短信，请速来电咨询。</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>适用范围</dt>
+                    <dd>全日制大专至全日制研究生（不分国籍，凭借有效学生证件刷码入园）</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>退改规则</dt>
+                    <dd>若需退改请提前一天晚上21：00前办理，逾期收取全额费用作为违约金。</dd>
+                </dl>
+                <a href="javascript:;" class="view-details">收起</a>
+            </div>
+        </div><!--//.tiptext-->
+    </dd><!--//.pdetails-->
+</dl>
+<dl class="ptditem">
+    <dd class="pdpaytype">
+        <span class="ipay ipay-local"><i class="itype">景区支付</i><a href="#" class="btn btn-w btn-small btn-orange">预订</a></span>
+    </dd>
+    <dd class="pdlvprice">
+        <dfn>&yen;<i>234</i></dfn>
+    </dd>
+    <dd class="pdprice"><del>&yen;308</del></dd>
+    <dt class="pdname">
+        <a href="javascript:;" class="ptlink">恐龙园圣诞夜 - 大学生票 (浙江、江苏大学生特惠)</a>
+    </dt>
+    <dd class="pdetails">
+        <div class="tiptext tip-light">
+            <div class="tip-arrow tip-arrow-11">
+                <em>◆</em>
+                <i>◆</i>
+            </div>
+            <div class="tip-other">
+                <dl class="dl-hor">
+                    <dt>入园须知</dt>
+                    <dd>1.营业时间：8:00—17:00（16:00停止换票）
+                    取票时间：8：30—17：00<br>
+                    2.景区地址：上海市浦东新区南六公路178号取票地点：上海市松江佘山上海欢乐谷检票处电子门票专用通道。<br>
+                    3.入园凭证：刷二维码短信入园。短信游玩当日有效，不能删除、不能转发。如您在出行前一天17：00尚未收到短信，请速来电咨询。</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>适用范围</dt>
+                    <dd>全日制大专至全日制研究生（不分国籍，凭借有效学生证件刷码入园）</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>退改规则</dt>
+                    <dd>若需退改请提前一天晚上21：00前办理，逾期收取全额费用作为违约金。</dd>
+                </dl>
+                <a href="javascript:;" class="view-details">收起</a>
+            </div>
+        </div><!--//.tiptext-->
+    </dd><!--//.pdetails-->
+</dl>
+<dl class="ptditem">
+    <dd class="pdpaytype">
+        <span class="ipay ipay-local"><i class="itype">景区支付</i><a href="#" class="btn btn-w btn-small btn-orange">预订</a></span>
+    </dd>
+    <dd class="pdlvprice">
+        <dfn>&yen;<i>234</i></dfn>
+    </dd>
+    <dd class="pdprice"><del>&yen;308</del></dd>
+    <dt class="pdname">
+        <a href="#" class="ptlink">恐龙园圣诞夜 - 大学生票 (浙江、江苏大学生特惠)如果标题很长很长很长很长很长很长怎么办</a>
+        <span class="tags tags-lightblue">折扣</span>
+    </dt>
+    <dd class="pdetails">
+        <div class="tiptext tip-light">
+            <div class="tip-arrow tip-arrow-11">
+                <em>◆</em>
+                <i>◆</i>
+            </div>
+            <div class="tip-other">
+                <dl class="dl-hor">
+                    <dt>入园须知</dt>
+                    <dd>1.营业时间：8:00—17:00（16:00停止换票）
+                    取票时间：8：30—17：00<br>
+                    2.景区地址：上海市浦东新区南六公路178号取票地点：上海市松江佘山上海欢乐谷检票处电子门票专用通道。<br>
+                    3.入园凭证：刷二维码短信入园。短信游玩当日有效，不能删除、不能转发。如您在出行前一天17：00尚未收到短信，请速来电咨询。</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>适用范围</dt>
+                    <dd>全日制大专至全日制研究生（不分国籍，凭借有效学生证件刷码入园）</dd>
+                </dl>
+                <dl class="dl-hor">
+                    <dt>退改规则</dt>
+                    <dd>若需退改请提前一天晚上21：00前办理，逾期收取全额费用作为违约金。</dd>
+                </dl>
+                <a href="javascript:;" class="view-details">收起</a>
+            </div>
+        </div><!--//.tiptext-->
+    </dd><!--//.pdetails-->
+</dl>
