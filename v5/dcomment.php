149d5df0be10f66116854a2e9c6bf7994b0b6c42
cloudAi <1395093509@qq.com>
2014-01-21 15:50:05 +0800

    修改

--- v5/dcomment.php
+++ v5/dcomment.php
@@ -1,4 +1,115 @@
 
+<!-- 点评信息概况 -->
 <div class="cominfo">
-    
-</div>
+    <div class="dscore">
+        <span class="comlevel"><dfn><em>好评率</em><i>96.7</i>%</dfn></span>
+        <span class="scorebox">
+            <p class="score-item">
+                <em>娱乐</em>
+                <span class="score-level"><i style="width:100%"></i></span>
+                <em>5.0</em>
+            </p>
+            <p class="score-item">
+                <em>安全</em>
+                <span class="score-level"><i style="width:90%"></i></span>
+                <em>4.5</em>
+            </p>
+            <p class="score-item">
+                <em>规模</em>
+                <span class="score-level"><i style="width:70%"></i></span>
+                <em>3.6</em>
+            </p>
+            <p class="score-item">
+                <em>人气</em>
+                <span class="score-level"><i style="width:60%"></i></span>
+                <em>6.2</em>
+            </p>
+        </span>
+    </div>
+    <div class="dtext">
+        <a href="#" class="btn btn-mini btn-w btn-orange">&nbsp;&nbsp;有订单，写点评，返现金&nbsp;&nbsp;</a>
+        <p>没订单？<a href="#" class="dlink">发表普通点评</a> 赠50积分，精华追加150积分！</p>
+    </div>
+</div><!--//.cominfo-->
+
+<!-- 点评详情 -->
+<div class="dcomment">
+    <div class="tab-dcom">
+        <ul class="ul-hor">
+            <li class="selected"><a href="javascript:;">体验点评<span>（15893）</span></a></li>
+            <li><a href="javascript:;">精华点评<span>（88）</span></a></li>
+            <li><a href="javascript:;">普通点评<span>（688）</span></a></li>
+        </ul>
+    </div>
+    <div class="dcombox JS_tabsbox">
+        <div class="tabcon selected">
+            <div class="dcom-item">
+                <div class="feedbox">
+                    <div class="feed-info">
+                        <span class="feed-date">2014-01-21</span>
+                        <span class="feed-user">user20140220</span>
+                        <p class="feed-score">
+                            <span class="feed-fav">好评</span>
+                            <span class="feed-item">娱乐 5(很好)</span>
+                            <span class="feed-item">安全 5(好)</span>
+                            <span class="feed-item">规模 5(很好)</span>
+                            <span class="feed-item">人气 5(好)</span>
+                        </p>
+                    </div>
+                    <div class="dbackinfo">
+                        <span class="tagsback tagsback-blue"><em>退</em><i>&yen;20</i></span>
+                        <span class="tagsback tagsback-green"><em>送</em><i>150积分</i></span>
+                    </div>
+                    <div class="dcom-info">
+                        <p class="dcom-text"></p>
+                    </div>
+                    <p class="dfeeduser">
+                        <a href="javascript:;" class="dcomplus"><i class="icon dicon-plus"></i><em>5</em></a>
+                        <a href="javascript:;" class="dcomuser"><i class="icon dicon-dcom"></i><em>1</em></a>
+                    </p>
+                </div><!---//.feedbox-->
+                <div class="feed-discuss">
+                    <div class="tiptext tip-default">
+                        <div class="tip-arrow tip-arrow-1">
+                            <em>◆</em>
+                            <i>◆</i>
+                        </div>
+                        <div class="tip-other">
+                            <div class="feeds-reply-box">
+                                <p>回复 lv138*******</p>
+                                <textarea class="textarea"></textarea>
+                                <button class="btn btn-small btn-w btn-orange">回复</button>
+                            </div>
+                            <ul class="feed-comments j-feed-comments">
+                                <li class="comment-item">
+                                    <p class="lv-recomment">
+                                        <span>驴妈妈回复：</span>你好，请咨询客服处理，感谢您的关注！
+                                    </p>
+                                </li>
+                                <li class="comment-item">
+                                    <p>
+                                        <span>lv133********回复：</span>是啊，我也超级喜欢古木游龙项目，体验超刺激，玩了还想玩。
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!--//.feed-discuss-->
+            </div><!--//.dcom-item-->
+        </div><!--//.tabcon-->
+        
+        <div class="tabcon">
+            <div class="dcom-item">
+                
+            </div>
+        </div><!--//.tabcon-->
+        
+        <div class="tabcon">
+            <div class="dcom-item">
+                
+            </div>
+        </div><!--//.tabcon-->
+        
+    </div><!--//.dcombox-->
+</div><!--//.dcomment-->
+
