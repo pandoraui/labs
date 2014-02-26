e83a675d6eb028ace082bec9b639c9bc9b4b026c
cloudAi <1395093509@qq.com>
2014-02-26 16:52:18 +0800

    新增页面

---
+++ v5/js/tuangou.js
@@ -0,0 +1,54 @@
+/*获取元素位置*/
+function elt_position(elt,position,fixed){
+    if(elt.length){
+        var fixed = (fixed ? $(document).scrollTop() :0);
+        var elt_pos = elt.offset();
+        switch(position){
+            case "top":
+                return elt_pos.top - fixed;
+                break;
+            case "bottom":
+                return elt_pos.top + elt.outerHeight(true);
+                break;
+            case "left":
+                return elt_pos.left;
+                break;
+            case "right":
+                return elt_pos.top + elt.outerWidth(true) -fixed;
+                break;
+            default :
+                return elt_pos.top;
+            ;
+        }
+    }else{
+        return -1; //不存在该元素
+    }
+}
+
+// promo Create:2012-3-26 for 方便控制的图片切换插件
+(function(a){a.fn.extend({banner_thaw:function(b){b=a.extend({thumbObj:null,botPrev:null,botNext:null,thumbNowClass:"hover",thumbOverEvent:true,slideTime:1000,autoChange:true,clickFalse:true,overStop:true,changeTime:5000,delayTime:300},b||{});var h=a(this);var i;var k=h.size();var e=0;var g;var c;var f;function d(){if(e!=g){if(b.thumbObj!=null){a(b.thumbObj).removeClass(b.thumbNowClass).eq(g).addClass(b.thumbNowClass)}if(b.slideTime<=0){h.eq(e).hide();h.eq(g).show()}else{h.eq(e).fadeOut(b.slideTime);h.eq(g).fadeIn(b.slideTime)}e=g;if(b.autoChange==true){clearInterval(c);c=setInterval(j,b.changeTime)}}}function j(){g=(e+1)%k;d()}h.hide().eq(0).show();if(b.thumbObj!=null){i=a(b.thumbObj);i.removeClass(b.thumbNowClass).eq(0).addClass(b.thumbNowClass);i.click(function(){g=i.index(a(this));d();if(b.clickFalse==true){return false}});if(b.thumbOverEvent==true){i.mouseenter(function(){g=i.index(a(this));f=setTimeout(d,b.delayTime)});i.mouseleave(function(){clearTimeout(f)})}}if(b.botNext!=null){a(b.botNext).click(function(){if(h.queue().length<1){j()}return false})}if(b.botPrev!=null){a(b.botPrev).click(function(){if(h.queue().length<1){g=(e+k-1)%k;d()}return false})}if(b.autoChange==true){c=setInterval(j,b.changeTime);if(b.overStop==true){h.mouseenter(function(){clearInterval(c)});h.mouseleave(function(){c=setInterval(j,b.changeTime)})}}}})})(jQuery);
+
+
+$(function(){
+    function x_hover(box_hover,hover_class){
+        $(box_hover).hover(
+        function(){
+            $(this).addClass(hover_class);
+        },function(){
+            $(this).removeClass(hover_class);
+        })
+    };
+    //x_hover("dl.titem","titem-hover");
+    
+    $('dl.titem').hover(
+    function(){
+        $(this).addClass('titem-hover').find('.tintro').slideDown(200);
+    },function(){
+        $(this).removeClass('titem-hover').find('.tintro').stop().slideUp(300);
+    });
+    
+    
+    
+    
+    
+})
---
+++ v5/tuangou.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>团购页面</title>
+<meta name="keywords" content=""/>
+<meta name="description" content="">
+
+<!--生产线引用-->
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v5/base.css,/styles/v5/common.css,/styles/new_v/header-air.css" >
+<link rel="stylesheet" href="http://pic.lvmama.com/min/index.php?f=/styles/v5/modules/form.css,/styles/v5/modules/button.css,/styles/v5/modules/table.css,/styles/v5/modules/tags.css,/styles/v5/modules/tip.css,/styles/v5/modules/paging.css" />
+<link rel="stylesheet" href="css/tuangou.css" />
+
+</head>
+<body>
+<!-- 公共头部开始  -->
+<!-- 
+ * 此示例展示，暂时使用了js来展现，
+ * 但开发上线，务必请引用头部的那个公共模块，参考其他项目
+-->
+<script src="http://www.lvmama.com/zt/web/common/header.js"></script>
+<!-- 公共头部结束  -->
+
+<div class="hr_c"></div>
+
+<div class="wrap wrapbox1">
+    <div class="tmain clearfix">
+        
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        
+    </div><!--//.tmain-->
+    <div class="tside">
+        <div class="sidebox">
+            <div class="ttitle">
+                <h4 class="tit">0元抽奖</h4>
+                <span class="gray">每周一上午10：00更新</span>
+            </div>
+            <div class="tcontent">
+                <dl class="dl-hor">
+                    <dt><a href="#" class="text-cover"><img src="//placehold.it/145x95/ffd3af" alt="" /></a></dt>
+                    <dd>
+                        <a href="#" class="tlink">上海帝盛酒店超级豪华套餐（浦东新区店）</a>
+                        <p><dfn>&yen;<i>1000</i></dfn><span class="price">市场价：<del>&yen;1240</del></span></p>
+                        <p>
+                            <a href="#" class="btn btn-orange btn-w fr">抽奖</a>
+                            <span class="num"><em class="orange">1990</em>人参加</span>
+                        </p>
+                    </dd>
+                </dl>
+            </div>
+        </div>
+    </div><!--//.tside-->
+</div> <!-- //.wrapbox1 -->
+
+<div class="wrap wrapbox2">
+    <div class="tmain clearfix">
+        
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        
+    </div><!--//.tmain-->
+    <div class="tside">
+    </div><!--//.tside-->
+</div> <!-- //.wrapbox2 -->
+
+<div class="wrap wrapbox3">
+    <div class="tmain clearfix">
+        
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        
+    </div><!--//.tmain-->
+    <div class="tside">
+    </div><!--//.tside-->
+</div> <!-- //.wrapbox3 -->
+
+<div class="wrap wrapbox4">
+    <div class="tmain clearfix">
+        
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        <?php include("titem.html"); ?>
+        
+    </div><!--//.tmain-->
+    <div class="tside">
+    </div><!--//.tside-->
+</div> <!-- //.wrapbox4 -->
+
+
+
+<!--公共底部-->
+<script src="http://pic.lvmama.com/js/v4/copyright.js"></script>
+
+<!-- 频道公用js-->
+<script src="http://pic.lvmama.com/min/index.php?f=/js/new_v/jquery-1.7.2.min.js,/js/ui/lvmamaUI/lvmamaUI.js,/js/new_v/top/header-air_new.js"></script>
+<script src="js/tuangou.js"></script>
+
+
+</body>
+</html>
