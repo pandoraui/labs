024180baf8fac881b44280cbada9902251ad9bd9
cloudAi <1395093509@qq.com>
2014-03-05 09:54:22 +0800

    提交一些图片文件

---
+++ v4/js/lv-common.js
@@ -0,0 +1,34 @@
+/* IE低版本提醒 */
+/*
+<!--[if lt IE 8]>
+<div id="killie-bar" class="killie-bar">
+    您使用的浏览器版本过低，建议立即升级到 <a href="http://download.microsoft.com/download/1/6/1/16174D37-73C1-4F76-A305-902E9D32BAC9/IE8-WindowsXP-x86-CHS.exe" target="_blank" >Internet Explorer 8</a> 或 <a href="http://www.google.cn/intl/zh-CN/chrome/browser/" target="_blank" >Google Chrome</a>
+</div>
+<div id="killie-bar" class="killie-bar">
+    您现在使用的浏览器版本过低，可能会导致部分图片和信息的缺失。请立即 <a href="http://download.microsoft.com/download/1/6/1/16174D37-73C1-4F76-A305-902E9D32BAC9/IE8-WindowsXP-x86-CHS.exe" target="_blank" >免费升级</a> 或下载使用 <a href="http://www.google.cn/intl/zh-CN/chrome/browser/" target="_blank" >Google Chrome浏览器</a> ，安全更放心！
+</div>
+<style>
+.killie-bar {
+    height: 24px;
+    line-height: 1.8;
+    font-weight:normal;
+    text-align: center;
+    border-bottom:1px solid #fce4b5;
+    background-color:#FFFF9B;
+    color:#e27839;
+    position: relative;
+    font-size: 14px;
+    text-shadow: 0px 0px 1px #efefef;
+    padding: 5px 0;
+}
+.killie-bar a {
+    color:#08c;
+    text-decoration: none;
+}
+</style>
+<![endif]-->
+*/
+
+
+
+
